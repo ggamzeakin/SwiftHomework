36ebb133161057b909576835e0476222564a7a96
ggamzeakin <gmznr556@gmail.com>
2024-09-12 21:43:43 +0300

    Swifthomework

---
+++ ödev1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+var ilce = "Kadıköy"
+
+print(ilce)
+print("ilçe : \(ilce)")
+
+var kita = "Asya"
+
+print(kita)
+print("Kıta: \(kita)")
+
+var posta_kodu:Int = 42500
+
+print("Posta Kodu :\(posta_kodu)")
+
+
+var instaAdresi: String = "@gamzeakin"
+
+print("İnstagram Adresi : \(instaAdresi)")
+
+var faksAdresi: String = "Konya"
+
+print("Faks Adresi: \(faksAdresi)")
+
+var calistiginBolum : String = "Matematik"
+print("Çalıştığın Bölüm : \(calistiginBolum)")
+
+
+var urunMiktari : Int = 36
+print("Ürün Miktarı :\(urunMiktari)")
